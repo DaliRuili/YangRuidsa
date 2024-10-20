3c9cbdddcbd5db44fdbcb47973bd47b4e09efec9
DaliRuili <2267752500@qq.com>
2024-10-20 22:45:32 +0800

    11

---
+++ examples/Server/Hotfix/Outer/Authentication/AuthenticationComponentSystem.cs
@@ -0,0 +1,11 @@
+﻿using Fantasy.Model.Authentication;
+
+namespace Fantasy.Outer.Authentication;
+
+public static class AuthenticationComponentSystem
+{
+    public static void Reg(this AuthenticationComponent self ,string username,string password)
+    {
+        Log.Debug("");
+    }
+}
